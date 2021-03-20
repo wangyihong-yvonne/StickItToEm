362a6f2857cda39d03f027132382b5ffc533ae5c
yu <feng.yu1@northeastern.edu>
2021-03-20 12:32:26 -0700

    fix receive chats key

--- app/src/main/java/edu/neu/madcourse/stickittoem/ChatActivity.java
+++ app/src/main/java/edu/neu/madcourse/stickittoem/ChatActivity.java
@@ -168,7 +168,7 @@ public class ChatActivity extends AppCompatActivity {
         pushRefForSend.setValue(messageID);
 
         if (!receiver.equals(sender)) {
-            DatabaseReference pushRefForReceive = mDatabase.child("chats").child("send").child(receiver).child(sender).child("messages").push();
+            DatabaseReference pushRefForReceive = mDatabase.child("chats").child("receive").child(receiver).child(sender).child("messages").push();
             pushRefForReceive.setValue(messageID);
         }
 
