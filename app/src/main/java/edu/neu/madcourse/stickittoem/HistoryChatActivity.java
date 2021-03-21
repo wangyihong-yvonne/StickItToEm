db4ad096b06476acca007dca5999db9b4cf96261
yu <feng.yu1@northeastern.edu>
2021-03-20 17:10:07 -0700

    fix history bug

--- app/src/main/java/edu/neu/madcourse/stickittoem/HistoryChatActivity.java
+++ app/src/main/java/edu/neu/madcourse/stickittoem/HistoryChatActivity.java
@@ -37,7 +37,6 @@ public class HistoryChatActivity extends AppCompatActivity {
         sender = intent.getStringExtra("sender");
         receiver = intent.getStringExtra("receiver");
 
-
         Map<Long, List<String>> timeMessageIDsMap = new HashMap<>();
         mDatabase = FirebaseDatabase.getInstance().getReference();
 
@@ -47,8 +46,7 @@ public class HistoryChatActivity extends AppCompatActivity {
         mDatabase.child("chats").child("send").child(sender).child(receiver).child("messages").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if (!snapshot.exists()) {
-                } else {
+                if (snapshot.exists()) {
                     Map<String, Map<String, Object>> senderToReceiverMap = (Map<String, Map<String, Object>>) snapshot.getValue();
                     for (Map<String, Object> m : senderToReceiverMap.values()) {
                         // m e.g.: {"timestamp":123, "id":"randomuuid"}
@@ -57,76 +55,76 @@ public class HistoryChatActivity extends AppCompatActivity {
                         list.add((String) m.get("id"));
                         timeMessageIDsMap.put(time, list);
                     }
-
-                    // get received messages from the current receiver
-                    mDatabase.child("chats").child("receive").child(sender).child(receiver).child("messages").addListenerForSingleValueEvent(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(@NonNull DataSnapshot snapshot) {
-                            if (snapshot.exists()) {
-                                Map<String, Map<String, Object>> receiverToSenderMap = (Map<String, Map<String, Object>>) snapshot.getValue();
-
-                                // m e.g.: {"timestamp":123, "id":"randomuuid"}
-                                for (Map<String, Object> m : senderToReceiverMap.values()) {
-                                    // m e.g.: {"timestamp":123, "id":"randomuuid"}
-                                    long time = (Long) m.get("timestamp");
-                                    List<String> list = timeMessageIDsMap.getOrDefault(time, new ArrayList<>());
-                                    list.add((String) m.get("id"));
-                                    timeMessageIDsMap.put(time, list);
-                                }
-                            }
-
-                            List<String> messageIDs = new ArrayList<>();
-                            SortedSet<Long> keys = new TreeSet<>(timeMessageIDsMap.keySet());
-                            for (long key : keys) {
-                                messageIDs.addAll(timeMessageIDsMap.get(key));
-                            }
-
-                            // get each messages from db then display
-                            for (String messageID : messageIDs) {
-                                mDatabase.child("messages").child(messageID).addListenerForSingleValueEvent(new ValueEventListener() {
-                                    @Override
-                                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                                        if (!snapshot.exists()) {
-                                        } else {
-                                            String content = snapshot.child("content").getValue(String.class);
-                                            long timestamp = snapshot.child("timestamp").getValue(Long.class);
-                                            String curSender = snapshot.child("sender").getValue(String.class);
-                                            String curReceiver = snapshot.child("receiver").getValue(String.class);
-                                            ChatMessage message = new ChatMessage(curSender, curReceiver, content, timestamp);
-
-                                            String formattedTimestamp = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(timestamp);
-                                            StringBuilder cur = new StringBuilder();
-                                            if (message.getSender().equals(sender)) {
-                                                cur.append("You ").append(formattedTimestamp).append("\n");
-                                                cur.append(message.getContent());
-                                            } else {
-                                                cur.append(message.getSender()).append(" ").append(formattedTimestamp).append("\n");
-                                                cur.append(message.getContent());
-                                            }
-                                            cur.append("\n");
-                                            LinearLayout layout = (LinearLayout) findViewById(R.id.history_chat_linear_layout);
-                                            TextView textView = new TextView(HistoryChatActivity.this);
-                                            textView.setText(cur.toString());
-                                            layout.addView(textView);
-                                        }
-                                    }
-
-                                    @Override
-                                    public void onCancelled(@NonNull DatabaseError error) {
-
-                                    }
-
-                                });
-                            }
-
-                        }
-
-                        @Override
-                        public void onCancelled(@NonNull DatabaseError error) {
-
-                        }
-                    });
                 }
+
+                // get received messages from the current receiver
+                mDatabase.child("chats").child("receive").child(sender).child(receiver).child("messages").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.exists()) {
+                            Map<String, Map<String, Object>> receiverToSenderMap = (Map<String, Map<String, Object>>) snapshot.getValue();
+
+                            // m e.g.: {"timestamp":123, "id":"randomuuid"}
+                            for (Map<String, Object> m : receiverToSenderMap.values()) {
+                                // m e.g.: {"timestamp":123, "id":"randomuuid"}
+                                long time = (Long) m.get("timestamp");
+                                List<String> list = timeMessageIDsMap.getOrDefault(time, new ArrayList<>());
+                                list.add((String) m.get("id"));
+                                timeMessageIDsMap.put(time, list);
+                            }
+                        }
+
+                        List<String> messageIDs = new ArrayList<>();
+                        SortedSet<Long> keys = new TreeSet<>(timeMessageIDsMap.keySet());
+                        for (long key : keys) {
+                            messageIDs.addAll(timeMessageIDsMap.get(key));
+                        }
+
+                        // get each messages from db then display
+                        for (String messageID : messageIDs) {
+                            mDatabase.child("messages").child(messageID).addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                    if (!snapshot.exists()) {
+                                    } else {
+                                        String content = snapshot.child("content").getValue(String.class);
+                                        long timestamp = snapshot.child("timestamp").getValue(Long.class);
+                                        String curSender = snapshot.child("sender").getValue(String.class);
+                                        String curReceiver = snapshot.child("receiver").getValue(String.class);
+                                        ChatMessage message = new ChatMessage(curSender, curReceiver, content, timestamp);
+
+                                        String formattedTimestamp = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(timestamp);
+                                        StringBuilder cur = new StringBuilder();
+                                        if (message.getSender().equals(sender)) {
+                                            cur.append("You ").append(formattedTimestamp).append("\n");
+                                            cur.append(message.getContent());
+                                        } else {
+                                            cur.append(message.getSender()).append(" ").append(formattedTimestamp).append("\n");
+                                            cur.append(message.getContent());
+                                        }
+                                        cur.append("\n");
+                                        LinearLayout layout = (LinearLayout) findViewById(R.id.history_chat_linear_layout);
+                                        TextView textView = new TextView(HistoryChatActivity.this);
+                                        textView.setText(cur.toString());
+                                        layout.addView(textView);
+                                    }
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError error) {
+
+                                }
+
+                            });
+                        }
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
             }
 
             @Override
