5c2e615821e3ee6e5ace3fdc19f883b921536e0c
yu <feng.yu1@northeastern.edu>
2021-03-21 15:13:35 -0700

    add sanity check

--- app/src/main/java/edu/neu/madcourse/stickittoem/ChatActivity.java
+++ app/src/main/java/edu/neu/madcourse/stickittoem/ChatActivity.java
@@ -93,16 +93,18 @@ public class ChatActivity extends AppCompatActivity {
 
     }
 
-    public void onClick1(View view)
-    {
+    public void onClick1(View view) {
         TextView message = (TextView) findViewById(R.id.textView2_sticker);
         message.setText(((TextView) view).getText());
     }
 
     public void sendMessageToDevice(View type) {
         final TextView message = (TextView) findViewById(R.id.textView2_sticker);
-//        final EditText editMessageText = findViewById(R.id.edit_message);
         final String editMessage = message.getText().toString().trim();
+        if (editMessage.equals(getResources().getString(R.string.sticker_chosen))) {
+            Toast.makeText(ChatActivity.this, "Please select an emoji", Toast.LENGTH_SHORT).show();
+            return;
+        }
         sendMessageToDevice(sender, receiverToken, editMessage);
     }
 
