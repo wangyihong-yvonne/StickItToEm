822f4b93351fac2127f1766a96c82198d2558330
Yihong <71856144+yihongwang001@users.noreply.github.com>
2021-03-21 04:26:45 -0700

    Create StickerUserPair.java

---
+++ app/src/main/java/edu/neu/madcourse/stickittoem/StickerUserPair.java
@@ -0,0 +1,12 @@
+package edu.neu.madcourse.stickittoem;
+
+public class StickerUserPair {
+        public String sentByUsername;
+        public String stickerName;
+        public StickerUserPair(String sentByUsername, String stickerName) {
+            this.sentByUsername = sentByUsername;
+            this.stickerName = stickerName;
+        }
+    }
+
+
